1b478e46990630d07e51a1e142336acbb5611089
grigor-aramyan <grigori.aramyan@gmail.com>
2019-05-31 00:33:29 +0400

    fix rating stars rendering

--- src/components/RatingStars.js
+++ src/components/RatingStars.js
@@ -8,11 +8,11 @@ export default function RatingStars({starsCount}) {
 
     return(
         <div>
-            starsCount > 0 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />
-            starsCount > 1 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />
-            starsCount > 2 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />
-            starsCount > 3 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />
-            starsCount > 4 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />
+            {starsCount > 0 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />}
+            {starsCount > 1 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />}
+            {starsCount > 2 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />}
+            {starsCount > 3 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />}
+            {starsCount > 4 ? <img style={styles} src="images/star_filled.png" /> : <img style={styles} src="images/star_empty.png" />}
         </div>
     );
 }
